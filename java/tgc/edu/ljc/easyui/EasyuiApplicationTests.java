354a246a2ef74820935e2849887dfe90c2c37940
Dayang26 <hsyy26@qq.com>
2020-12-01 20:55:01 +0800

    123

---
+++ java/tgc/edu/ljc/easyui/EasyuiApplicationTests.java
@@ -0,0 +1,13 @@
+package tgc.edu.ljc.easyui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyuiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
